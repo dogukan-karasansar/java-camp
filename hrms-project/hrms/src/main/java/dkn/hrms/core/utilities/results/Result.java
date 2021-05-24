a69bb2825bb5ffb71e3094e99f0362b81f82014a
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-05-24 17:52:46 +0300

    resultlar oluşturuldu

---
+++ hrms-project/hrms/src/main/java/dkn/hrms/core/utilities/results/Result.java
@@ -0,0 +1,23 @@
+package dkn.hrms.core.utilities.results;
+
+public class Result {
+    private boolean success;
+    private String message;
+
+    public Result(boolean success) {
+        this.success = success;
+    }
+
+    public Result(boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ hrms-project/hrms/src/main/java/dkn/hrms/core/utilities/results/SuccessDataResult.java
@@ -0,0 +1,19 @@
+package dkn.hrms.core.utilities.results;
+
+public class SuccessDataResult<T> extends DataResult{
+    public SuccessDataResult(T data) {
+        super(data, true);
+    }
+
+    public SuccessDataResult(T data,  String message) {
+        super(data, true, message);
+    }
+
+    public SuccessDataResult() {
+        super(null,  true);
+    }
+
+    public SuccessDataResult(String message) {
+        super(null, true, message);
+    }
+}
